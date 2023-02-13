df88e7b8f8b50fd20b72706019706d423e980756
K-Othman <karim_othman88@hotmail.com>
2023-02-13 11:42:03 +0000

    finished setup  added routes and context

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import LogIn from "./pages/LogIn";
+import SignUp from "./pages/SignUp";
+import Store from "./pages/Store";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/store" element={<Store />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/login" element={<LogIn />} />
+        <Route path="/signin" element={<SignUp />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
