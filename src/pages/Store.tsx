52734571f9e0c0a8566470d2fbc2dc44bdb46872
K-Othman <karim_othman88@hotmail.com>
2023-02-13 14:52:38 +0000

    main styles for the products page

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -5,24 +5,46 @@ function Store() {
   const productsList = useContext(ProductsContext);
 
   return (
-    <div>
+    <section
+      className="container mx-auto pt-10 "
+      style={{
+        display: "grid",
+        gridTemplateColumns: "repeat(auto-fill, minmax(250px , 1fr))",
+        gap: "30px",
+      }}
+    >
       {productsList.products.map((product) => {
         return (
-          <section key={product.id} className="h-80 flex ">
-            <img src={product.image} alt={product.description} />
-            <div>
-              <p> {product.title} </p>
-              <p> {product.category} </p>
+          <div key={product.id} className="border bg-white p-1 ">
+            <img
+              className="h-[200px] m-auto "
+              src={product.image}
+              alt={product.description}
+              style={{ objectFit: "fill" }}
+            />
+            <div className="p-2  mx-auto ">
+              <p className="font-bold py-3 ">
+                {" "}
+                {product.title.slice(0, 30)}...{" "}
+              </p>
+              <p className="text-[#757F85] pb-2"> {product.category} </p>
               <div>
-                <p> {product.rating.count} </p>
+                <p className="text-red-500">
+                  {product.rating.count < 10 ? "Low Stock" : null}
+                </p>
                 <p> {product.rating.rate} </p>
               </div>
-              <p> {product.price} </p>
+              <p className="pt-2 text-[#757F85]"> £{product.price} </p>
             </div>
-          </section>
+            <div>
+              <button className="border w-[100%] m-auto rounded py-2 bg-[#1A6CDD] text-white ">
+                + Add To Cart
+              </button>
+            </div>
+          </div>
         );
       })}
-    </div>
+    </section>
   );
 }
 
