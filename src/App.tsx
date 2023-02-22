aecd4192f5bdb25b30083d0b42c640e44430af2a
K-Othman <karim_othman88@hotmail.com>
2023-02-22 18:00:02 +0000

    auth with email and password

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,18 @@
-import { useContext } from "react";
 import { Route, Routes } from "react-router-dom";
 import CartItems from "./components/CartItems";
 import Navbar from "./components/Navbar";
-import { CartContext } from "./context/ShoppingCartContext";
 import About from "./pages/About";
 import Home from "./pages/Home";
-import LogIn from "./pages/LogIn";
+import SignIn from "./pages/SignIn";
 import Product from "./pages/Product";
 import SignUp from "./pages/SignUp";
 import Store from "./pages/Store";
+// import {firebaseConfig} from "./firebase"
+// import { initializeApp } from "firebase/app";
+
+// initializeApp(.firebaseConfig)
 
 function App() {
-  // bring the context here and pass the cartItem id to the CartItems compmnent so you can use the id
-  // const { cartItems } = useContext(CartContext);
-  // console.log(cartItems, "CART");
-  // cartItems.map((item) => {
-  //   <CartItems key={item.id} {...item} />;
-  // });
-
   return (
     <>
       <Navbar />
@@ -28,7 +23,7 @@ function App() {
           <Route path="/store/:category" element={<Store />} />
           <Route path="/products/:productId" element={<Product />} />
           <Route path="/about" element={<About />} />
-          <Route path="/login" element={<LogIn />} />
+          <Route path="/login" element={<SignIn />} />
           <Route path="/signin" element={<SignUp />} />
           <Route path="/carts" element={<CartItems />} />
         </Routes>
